c1d1e5b8d9ab91f91bdce8f14b58e975787db771
Sebast1anG <sebastian.garas66@gmail.com>
2021-07-21 18:36:43 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,20 +14,41 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // ** UPcoming Task *//
 
-// Get all the upcoming task
-
+Route::get("/upcoming", function(){
+    $upcoming = Upcoming::all();
+    
+    return UpcomingResource::collection($upcoming);
+});
 // Add a new task
-
+Route::post("/upcoming", function(Request $request){
+    return Upcoming::create([
+        'title' => $request->title,
+        'taskId' => $request->taskID,
+        'waiting' => $request->waiting
+     ]);
+});
 
 // Delete upcoming task
 
+Route::delete("/upcoming/{taskId}", function(Request $request){
+    DB::table('upcomings')->where('taskId', $taskId)->delete();
+    
+    return 204;
+});
 //** Today Task */
 
 // Get all task
-
-
-// Add a task
-
-
-// Delete Today Task
+Route::get("/dailyTask", function(){
+    $todaytask = Today::all();
+    return TodayTaskResource::colection($todaytask);
+});
+//Add a task
+    Route::post('/dailytask', function (Request $request) {
+        return Today::create([
+        'id' => $request->id,
+        'title' => $request->title,
+        'taskid' => $request->taskID
+         ]);
+  ]);
+ 
 
